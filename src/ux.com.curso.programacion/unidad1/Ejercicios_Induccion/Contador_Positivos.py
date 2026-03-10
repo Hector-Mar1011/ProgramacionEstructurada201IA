b140d734cfe2edb09a6139269cce6273e226bb70
Héctor <hectorgarcia1893@hotmail.com>
2026-03-10 07:38:16 -0600

    Contador de Positivos

--- src/ux.com.curso.programacion/unidad1/Ejercicios_Induccion/Contador_Positivos.py
+++ src/ux.com.curso.programacion/unidad1/Ejercicios_Induccion/Contador_Positivos.py
@@ -1,7 +1,10 @@
 #Desarrollo de algoritmo contador de positivos
 
 # Declaración de variables
-
+"""
+Esta función tiene como tarea contar los numeros hasta que el usuario decida 
+romper con el bucle poniendo un numero no positivo y reiniciar desde cero el contador.
+"""
 def contador_positivos():
     contador = 0 
     while True:
@@ -13,11 +16,15 @@ def contador_positivos():
     print("Cantidad de números positivos ingresados: ", contador)
 
 #Definición de la función main (Controla el flujo del programa)
+"""
+Esta función imprime un texto en pantalla para el inicio del contador, despues
+llama a la función del contador para ejecutar el bucle.
+"""
 def main():
     print("Bienvenido al contador de positivos")
     contador_positivos()
 
 #Llamada a la función para iniciar el programa
 
-if __name__ == "__main__":
+if __name__ == "__main__": # Llama a la funcion main para iniciar con la ejecución del codigo
     main()
