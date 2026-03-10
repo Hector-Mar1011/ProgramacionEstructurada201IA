a3eae7d4ef21427af7298c70e0c9ae730abb6aea
Héctor <hectorgarcia1893@hotmail.com>
2026-03-10 10:24:44 -0600

    Clasificacion de Calificaciones

---
+++ src/ux.com.curso.programacion/unidad1/Tipo_Dados.py
@@ -0,0 +1,29 @@
+def dato():
+    # Demostración de tipo de datos en python
+
+    entero = 25
+    decimal = 3.14
+    cadena = "Hola Mundo!"
+    booleano = True
+
+    print(entero)
+    print(decimal)
+    print(cadena)
+    print(booleano)
+
+def tipos_datos_compuestos():
+    lista =[10,20,30,40]
+    Tupla = (19,29,39,49)
+    diccionario = {"Nombre":"juan", "Edad": 30, "Ciudad": "Madrid"}
+
+    print(lista)
+    print(Tupla)
+    print(diccionario)
+
+def main():
+    dato()
+    tipos_datos_compuestos()
+    
+    
+if __name__ == "__main__":
+    main()
