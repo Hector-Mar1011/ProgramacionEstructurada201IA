df6e9b8ab00b92517a3f25daad939da2f879e0e9
Héctor <hectorgarcia1893@hotmail.com>
2026-03-10 07:51:06 -0600

    Identificadores

---
+++ src/ux.com.curso.programacion/unidad1/Identificadores.py
@@ -0,0 +1,24 @@
+def imprimir_identificadores():
+    # Identificadores validos
+    nombre_usuario = "Alumno" # Inicia con letra y tiene guion bajo
+    sensor = "Temperatura"  # Inicia con letra
+    _id_interno =12 # Puede contener guion bajo y números
+
+    print(nombre_usuario)
+    print(sensor)
+    print(_id_interno)
+
+
+# Nombre correcto de funciones
+def calcular_area():
+    print("Calculando el área...")
+
+def main():
+    imprimir_identificadores()
+    calcular_area()
+
+if __name__ == "__main__":
+    main()
+    
+
+
