3d01e4a668a2721e286c7eca2f7ff687c891b0a8
Héctor <hectorgarcia1893@hotmail.com>
2026-03-10 08:02:59 -0600

    Nivel de Identación

---
+++ src/ux.com.curso.programacion/unidad1/nivel_identacion.py
@@ -0,0 +1,24 @@
+# Ejemplo para visualizar la identación en python
+
+def explicar_identacion():
+    # Nivel 1
+    mensaje = "Nivel 1 de identación"
+    print(mensaje)
+
+    puntos = 10
+    if puntos >9:
+        #Nivel 2 
+        print("Entra al flujo if")
+
+        if puntos == 10:
+            #Nivel 3
+            print("Puntos es igual a 10")
+    
+    #Cierra nivel 1
+
+def main():
+    explicar_identacion()
+    
+if __name__ == "__main__":
+    main()
+
