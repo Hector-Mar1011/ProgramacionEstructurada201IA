6d61081fc82dfc1b2824577bba8725d21a15b4f1
Héctor <hectorgarcia1893@hotmail.com>
2026-04-14 07:50:01 -0600

    Creacion de archivo paso a paso para mantenimiento del codigo v2

--- src/ux.com.curso.programacion/unidad3/Ejercicios_IA/Practica_02/AnalizadorPredictivo.py
+++ src/ux.com.curso.programacion/unidad3/Ejercicios_IA/Practica_02/AnalizadorPredictivo.py
@@ -16,12 +16,20 @@ def limpiar_dato(lectura):
         return None
     
 def calcular_alerta(valor_normalizado):
- """
- FUNCIÓN 2: Recibe el valor (0.0 a 1.0).
- Devuelve 'CRÍTICO' si es > 0.8, 'PRECAUCIÓN' si es > 0.5,
- y 'NORMAL' en cualquier otro caso.
- """
- # IMPLEMENTAR AQUÍ
+    """
+    FUNCIÓN 2: Recibe el valor (0.0 a 1.0).
+    Devuelve 'CRÍTICO' si es > 0.8, 'PRECAUCIÓN' si es > 0.5,
+    y 'NORMAL' en cualquier otro caso.
+    """
+    # IMPLEMENTAR AQUÍ
+    if valor_normalizado > 0.8:
+       return 'CRÍTICO'
+    elif valor_normalizado > 0.5:
+       return 'PRECAUCIÓN'
+    else:
+       return 'NORMAL'
+    
+
 def obtener_estadisticas(lista_datos):
  """
  FUNCIÓN 3: Recibe la lista de datos válidos.
