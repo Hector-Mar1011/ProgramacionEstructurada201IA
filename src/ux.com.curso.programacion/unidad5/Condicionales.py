faf0c8788d7c40b2a3193034b568067e7f17a7a7
Héctor <hectorgarcia1893@hotmail.com>
2026-05-26 08:20:11 -0600

    Correccion de error if-else

--- src/ux.com.curso.programacion/unidad5/Condicionales.py
+++ src/ux.com.curso.programacion/unidad5/Condicionales.py
@@ -4,19 +4,21 @@ En este codigo se van a usar condicionales if-else para determinar si un numero 
 
 # Creamos una funcion que almacene el proceso de verificación.
 def verificacion():
-    # Pedimos al usuario que ingrese un numero
-    numero = int(input("Ingrese un numero: "))
-    # Verificamos si el numero es par o impar
-    if numero % 2 == 0:
-        print("Not Weird.")
-    elif numero % 2 == 0 and  numero >= 2 and numero <= 5:
-        print("Not Weird.")
-    elif numero % 2 == 0 and numero >= 6 and numero <= 20:
-        print("Weird.")
-    elif numero % 2 == 0 and numero > 20:
-        print("Not Weird.")
+    n= int(input("Ingrese un numero: "))
+  # 1. Si n es impar, imprime Weird
+    if n % 2 != 0:
+        print("Weird")
     else:
-        print("Weird.")
+        # Si entra aquí, ya sabemos que n es par
+        # 2. Si es par y está entre 2 y 5 (inclusive), imprime Not Weird
+        if 2 <= n <= 5:
+            print("Not Weird")
+        # 3. Si es par y está entre 6 y 20 (inclusive), imprime Weird
+        elif 6 <= n <= 20:
+            print("Weird")
+        # 4. Si es par y es mayor que 20, imprime Not Weird
+        elif n > 20:
+            print("Not Weird")
 
 
 # Llamamos a la funcion para ejecutar el proceso
@@ -24,7 +26,7 @@ def main():
     while True:
         verificacion()
     
-    if numero == string:
+    if n == str(n):
         print("Ingrese un numero valido.")
 
 
