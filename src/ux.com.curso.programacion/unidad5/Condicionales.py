a84c375a7acb2d4bba12ae108a31d90dd57fe560
Héctor <hectorgarcia1893@hotmail.com>
2026-05-26 07:49:29 -0600

    Python if-Else Actividad en clase

---
+++ src/ux.com.curso.programacion/unidad5/Condicionales.py
@@ -0,0 +1,26 @@
+"""
+En este codigo se van a usar condicionales if-else para determinar si un numero es par o impar.
+"""
+
+# Creamos una funcion que almacene el proceso de verificación.
+def verificacion():
+    # Pedimos al usuario que ingrese un numero
+    numero = int(input("Ingrese un numero: "))
+    # Verificamos si el numero es par o impar
+    if numero % 2 == 0:
+        print("Not Weird.")
+    else:
+        print("Weird.")
+
+
+# Llamamos a la funcion para ejecutar el proceso
+def main():
+    while True:
+        verificacion()
+    
+    if numero == string:
+        print("Ingrese un numero valido.")
+
+
+if __name__ == "__main__":
+    main()  
