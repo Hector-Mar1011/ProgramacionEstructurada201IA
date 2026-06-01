e4f3e296dd45698360ea2983a31c1481393d97d0
Héctor <hectorgarcia1893@hotmail.com>
2026-06-01 07:53:43 -0600

    FIX version final

--- src/ux.com.curso.programacion/unidad5/Practica01/Control_Ventas/Control_Ventas.py
+++ src/ux.com.curso.programacion/unidad5/Practica01/Control_Ventas/Control_Ventas.py
@@ -10,9 +10,8 @@ productos = ["Laptop", "Smartphone", "Tablet"]
 
 ventas = [[0] * 3 for _ in range(3)]
 
-total_general = 0
 
-total_por_producto = [] 
+total_por_producto = [0] * 3
 
 # Lectura de datos
 def leer_ventas():
@@ -26,24 +25,26 @@ def leer_ventas():
 
  
 def reporte_ventas():
+    total_general = 0
+
     for i in range(3):
 
         suma_producto = sum(ventas[i])
-        total_por_producto.append(suma_producto)
+        total_por_producto[i] = suma_producto
         total_general += suma_producto
 
         print(f"{productos[i]}: {ventas[i]} | Total: {suma_producto}")
 
  
 
-print(f"\nEl total de ventas de la semana es: {total_general}")
+    print(f"\nEl total de ventas de la semana es: {total_general}")
 
-print(f"El promedio de ventas es: {total_general / 9:.2f}")
+    print(f"El promedio de ventas es: {total_general / 9:.2f}")
 
-# Producto mas vendido
+    # Producto mas vendido
 
-index_max = total_por_producto.index(max(total_por_producto))
-print(f"El producto más vendido es: {productos[index_max]} con {total_por_producto[index_max]} ventas.")       
+    index_max = total_por_producto.index(max(total_por_producto))
+    print(f"El producto más vendido es: {productos[index_max]} con {total_por_producto[index_max]} ventas.")       
 
 
 def main():
