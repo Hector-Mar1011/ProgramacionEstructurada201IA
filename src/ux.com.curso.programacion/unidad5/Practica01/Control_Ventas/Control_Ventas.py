d5896b8bfbb4ecbfee40ef5a6b8ee6eb956b230a
Héctor <hectorgarcia1893@hotmail.com>
2026-06-01 07:41:57 -0600

    UPDATE

--- src/ux.com.curso.programacion/unidad5/Practica01/Control_Ventas/Control_Ventas.py
+++ src/ux.com.curso.programacion/unidad5/Practica01/Control_Ventas/Control_Ventas.py
@@ -11,7 +11,8 @@ productos = ["Laptop", "Smartphone", "Tablet"]
 ventas = [[0] * 3 for _ in range(3)]
 
 total_general = 0
- 
+
+total_por_producto = [] 
 
 # Lectura de datos
 def leer_ventas():
@@ -28,7 +29,7 @@ def reporte_ventas():
     for i in range(3):
 
         suma_producto = sum(ventas[i])
-
+        total_por_producto.append(suma_producto)
         total_general += suma_producto
 
         print(f"{productos[i]}: {ventas[i]} | Total: {suma_producto}")
@@ -39,17 +40,15 @@ print(f"\nEl total de ventas de la semana es: {total_general}")
 
 print(f"El promedio de ventas es: {total_general / 9:.2f}")
 
+# Producto mas vendido
+
+index_max = total_por_producto.index(max(total_por_producto))
+print(f"El producto más vendido es: {productos[index_max]} con {total_por_producto[index_max]} ventas.")       
+
+
 def main():
-    
-
-    print("\nRESUMEN DE VENTAS")
-
-    total_general = 0
-
-    print(f"\nEl total de ventas de la semana es: {total_general}")
-
-    print(f"El promedio de ventas es: {total_general / 9:.2f}")
-
+    leer_ventas()
+    reporte_ventas()
 
  
 
